e2a4beb2c9ad37a1114277cb057dc021922e8e5f
Tammy <twelveblackpaws@gmail.com>
2011-05-22 12:49:14 -0700

    Added error handling

--- flickr2tumblr.py
+++ flickr2tumblr.py
@@ -4,6 +4,11 @@ import flickrapi
 import sys
 
 api_key = "b5357a20f386908fa7a51f382a744bc2"
+
+if len(sys.argv) != 3:
+    print "Usage: flickr2tumblr <photo_id> <photo_size>"
+    sys.exit(-1) 
+
 photo_id = sys.argv[1]
 photo_size = sys.argv[2]
 
@@ -14,16 +19,25 @@ flickr = flickrapi.FlickrAPI(api_key)
 
 infoElem = flickr.photos_getInfo(photo_id=photo_id)
 if infoElem.attrib['stat']!='ok':
+    print "Unable to get info from photo_id", photo_id
     sys.exit(-1)
+    
 url = infoElem.find('photo').find('urls').findall('url')[0].text
 
 sizesElem = flickr.photos_getSizes(photo_id=photo_id)
 if sizesElem.attrib['stat']!='ok':
+    print "Unable to get sizes for photo_id", photo_id
     sys.exit(-1)
 
 sizes = sizesElem.find('sizes').findall('size')
+source = None
 for size in sizes:
     if size.attrib['label'] == photo_size:
         source = size.attrib['source']
-        
+  
+if source is None:
+    print photo_size,"is not defined for photo",photo_id
+    die(-1)
+    
+    
 print photo_id,url,photo_size,source
