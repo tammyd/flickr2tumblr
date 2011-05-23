38244b49115c98c89d99497125442958eba6ec47
Tammy <twelveblackpaws@gmail.com>
2011-05-22 20:14:07 -0700

    Added ability to add custom tags to the post. Pulled all constants out into a config file

--- flickr2tumblr.py
+++ flickr2tumblr.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python
 
 import flickrapi
+from tumblr import Api
 import sys
+import ConfigParser
 
-api_key = "b5357a20f386908fa7a51f382a744bc2"
+cfgParser = ConfigParser.ConfigParser()
+cfgParser.read('config.ini')
+cfgSettings = {'flickr':['api_key'], 'tumblr':['blog','email','password']}
+config = {}
+for cat,key in cfgSettings.iteritems():
+    for value in key:
+        try:
+            config["%s_%s" % (cat,value)] = cfgParser.get(cat,value)
+        except:
+            print "Error in config file - missing or invalid %s %s" % (cat,value)
+            sys.exit(-1)
 
-if len(sys.argv) != 3:
-    print "Usage: flickr2tumblr <photo_id> <photo_size>"
+if len(sys.argv) < 3:
+    print "Usage: flickr2tumblr <photo_id> <photo_size> [tags]"
     sys.exit(-1) 
 
 photo_id = sys.argv[1]
 photo_size = sys.argv[2]
 
-#photo_id = 5645771097;
-#size = 'Medium';
-
-flickr = flickrapi.FlickrAPI(api_key)
+flickr = flickrapi.FlickrAPI(config['flickr_api_key'])
 
 infoElem = flickr.photos_getInfo(photo_id=photo_id)
 if infoElem.attrib['stat']!='ok':
@@ -36,8 +45,28 @@ for size in sizes:
         source = size.attrib['source']
   
 if source is None:
-    print photo_size,"is not defined for photo",photo_id
+    print photo_size,"is a not defined size for photo",photo_id
     die(-1)
     
+
+tumblrArgs = {}
+tumblrArgs['state'] = 'queue'
+tumblrArgs['type'] = 'photo'
+tumblrArgs['source'] = source
+tumblrArgs['click-through-url'] = url
+tumblrArgs['tags'] = 'cat,"black cat"'
+try:
+    tags = sys.argv[3]
+    tumblrArgs['tags'] = tumblrArgs['tags'] + "," + tags
+except:
+    #do nothing
+    tumblrArgs['tags'] = tumblrArgs['tags']
     
-print photo_id,url,photo_size,source
+api = Api(config['tumblr_blog'],config['tumblr_email'],config['tumblr_password'])
+try:
+    post = api._write(tumblrArgs)
+    print "Post Queued!"
+except:
+    print "Error queuing post"
+    
+    
